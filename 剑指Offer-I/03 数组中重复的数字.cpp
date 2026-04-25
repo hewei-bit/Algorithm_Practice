85dd03d3a443dce47bf7b34f437575dd0fb6f446
hewei-bit <1003826976@qq.com>
2026-04-25 11:58:31 +0800

    20260425 再次启航

--- 剑指Offer-I/03 数组中重复的数字.cpp
+++ 剑指Offer-I/03 数组中重复的数字.cpp
@@ -21,15 +21,21 @@ using namespace std;
 class Solution
 {
 public:
+    // 定义一个函数，用于找出数组中重复的数字
     int findRepeatNumber(vector<int> &nums)
     {
+        // 定义一个无序映射，用于存储数组中的数字
         unordered_map<int, bool> res;
+        // 遍历数组中的每个数字
         for (int num : nums)
         {
+            // 如果该数字已经存在于映射中，则说明该数字重复，返回该数字
             if (res[num])
                 return num;
+            // 否则将该数字添加到映射中
             res[num] = true;
         }
+        // 如果数组中没有重复的数字，则返回-1
         return -1;
     }
 };

4086b97d6ef63d452bf66f9315589f5638a2078e
hewei-bit <1003826976@qq.com>
2026-04-29 00:15:51 +0800

    feat: 添加删除有序数组中重复项的解法

---
+++ leetcode/leetcode算法学习/数组和字符串/26 删除有序数组中的重复项.cpp
@@ -0,0 +1,36 @@
+/**
+ * @File Name: 27 删除有序数组中的重复项.cpp
+ * @Brief :
+ * @Author : hewei (hewei_1996@qq.com)
+ * @Version : 1.0
+ * @Creat Date : 2024-01-13
+ *
+ */
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        int slow = 1,fast = 1;
+        while(fast < n){
+            if(nums[fast] != nums[fast-1])
+            {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow ;
+    }
+};
